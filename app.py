cf51de4bb37720fd034923f3773e094fc6f7479d
Renato S. Thiago <renatosts@gmail.com>
2022-06-05 15:38:11 -0300

    Atualização automática

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-import streamlit as st
+from bs4 import BeautifulSoup
+from datetime import datetime
+from pandas_datareader import data as pdr
+from plotly.subplots import make_subplots
+from zipfile import ZipFile
+import csv
+import matplotlib.pyplot as plt
+import os
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
-from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-from pandas_datareader import data as pdr
+import re
+import requests as req
+import streamlit as st
 import yfinance as yf
 
-# SETTING PAGE CONFIG TO WIDE MODE
-st.set_page_config(
-    layout='wide',
-    page_icon='app.jpg',
-    page_title='B3')
 
 # Read CSV Dados Financeiros
 
@@ -41,8 +43,321 @@ def define_color(val):
     return 'color: %s' % color
 
 
-f ='https://raw.githubusercontent.com/renatosts/AnaliseFundamentalista/main/DadosFinanceiros.csv'
-#f = r'C:\Users\Renato\Documents\_Projetos Github\CVM_Dados_Financeiros\DadosFinanceiros.csv'
+def cria_cadastro():
+
+    # Determina Cadastro
+    cadastro = processa_base_cvm('FCA', 'geral')
+    cadastro  = cadastro.groupby('CNPJ_Companhia')[['Codigo_CVM', 'Nome_Empresarial', 'Setor_Atividade', 'Pagina_Web']].last().reset_index()
+    cadastro.columns = ['cnpj', 'cod_cvm', 'nome', 'segmento', 'site']
+    cadastro.segmento = cadastro.segmento.str.replace('Emp. Adm. Part. - ', '', regex=False)
+    cadastro.segmento = cadastro.segmento.str.replace('Emp. Adm. Part.-', '', regex=False)
+    cadastro.nome = cadastro.nome.str.upper()
+
+    # Determina tickers de negociação
+    tickers = processa_base_cvm('FCA', 'valor_mobiliario')
+    tickers = tickers[['CNPJ_Companhia', 'Codigo_Negociacao']].drop_duplicates().dropna()
+    tickers = tickers[~tickers.Codigo_Negociacao.str.contains('*', regex=False)]
+    tickers = tickers.groupby(['CNPJ_Companhia']).agg({'Codigo_Negociacao': ','.join}).reset_index()
+    tickers.columns = ['cnpj', 'ticker']
+    tickers.ticker = tickers.ticker.str.upper()
+
+    cadastro = cadastro.merge(tickers, on='cnpj')
+
+
+    # Quantidade de ações
+    distribuicao_capital = processa_base_cvm('FRE', 'distribuicao_capital')
+    distribuicao_capital = distribuicao_capital[distribuicao_capital['Quantidade_Total_Acoes_Circulacao'] > 0]
+    distribuicao_capital = distribuicao_capital[distribuicao_capital['Percentual_Total_Acoes_Circulacao'] > 0]
+    distribuicao_capital = distribuicao_capital.groupby('CNPJ_Companhia')[['Data_Referencia', 'Versao', 'Quantidade_Total_Acoes_Circulacao', 'Percentual_Total_Acoes_Circulacao']].last().reset_index()
+    distribuicao_capital['acoes'] = (distribuicao_capital.Quantidade_Total_Acoes_Circulacao / (distribuicao_capital.Percentual_Total_Acoes_Circulacao / 100)).astype('int64')
+    distribuicao_capital.columns = ['cnpj', 'dt_ref', 'versao', 'acoes_circul', 'free_float', 'acoes']
+    distribuicao_capital = distribuicao_capital[['cnpj', 'acoes', 'free_float']]
+
+    cadastro = cadastro.merge(distribuicao_capital, on='cnpj', how='left')
+    cadastro['acoes'] = cadastro['acoes'].fillna(0).astype('int64')
+
+
+    # Governança
+    governanca = processa_base_cvm('FCA', 'valor_mobiliario')
+    governanca = governanca.groupby('CNPJ_Companhia')[['Data_Referencia', 'Versao', 'Segmento']].last().reset_index()
+    governanca = governanca[['CNPJ_Companhia', 'Segmento']]
+    governanca.columns = ['cnpj', 'governanca']
+
+    cadastro = cadastro.merge(governanca, on='cnpj', how='left')
+
+    return cadastro
+
+
+def download_arquivos_CVM(tipo):
+
+    # Acessa site CVM para verificar arquivos anuais para download
+
+    URL_CVM = f'http://dados.cvm.gov.br/dados/CIA_ABERTA/DOC/{tipo}/DADOS/'
+
+    # Verifica data do último download
+
+    with open('controle_download.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data_ultimo_download = row[0]
+
+    # Acessa site CVM para verificar arquivos mensais para download
+
+    try:
+
+        resp = req.get(URL_CVM)
+
+    except Exception as e:
+
+        print(e)
+
+
+    bs = BeautifulSoup(resp.text, 'html.parser')
+
+    pre = bs.find('pre')
+
+    nome = []
+    for m in re.finditer(rf"{tipo.lower()}_cia_aberta_\w*.zip", pre.text):  
+        nome.append(m.group(0).rstrip('.zip'))
+    
+    last_mod = []
+    for m in re.finditer(r"\d{2}-\w{3}-\d{4} \w{2}:\w{2}", pre.text):  
+        last_mod.append(m.group(0))
+
+    # df contém a lista dos arquivos a serem baixados
+    df = pd.DataFrame()
+    df['nome'] = nome
+    df['last_mod'] = last_mod
+    df['ano'] = df.nome.str[15:19]
+    df['last_mod'] = pd.to_datetime(df.last_mod)
+
+    df = df[df['last_mod'] > data_ultimo_download]
+
+    for arq in df['nome']:
+         print('Download do arquivo:', arq)
+         download_url(URL_CVM + arq + '.zip', dest_folder=rf'Base_CVM\{tipo}')
+
+
+def download_url(url: str, dest_folder: str):
+
+
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # create folder if it does not exist
+
+    filename = url.split('/')[-1].replace(' ', '_')  # be careful with file names
+    file_path = os.path.join(dest_folder, filename)
+
+    r = req.get(url, stream=True)
+    if r.ok:
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:  # HTTP status code 4XX/5XX
+        print('Download failed: status code {}\n{}'.format(r.status_code, r.text))
+
+
+def processa_base_cvm(tipo, arquivo):
+
+    primeiro_ano = datetime.today().year - 9
+
+    df = pd.DataFrame()
+
+    pasta = f'Base_CVM\\{tipo}\\'
+
+    for filezip in os.listdir(pasta):
+
+        ano = int(re.search('[0-9]+', filezip).group(0))
+
+        if ano >= primeiro_ano:
+
+            with ZipFile(pasta + filezip) as zip:
+
+                nome_arq = f'{tipo.lower()}_cia_aberta_{arquivo}_{ano}.csv'
+                with zip.open(nome_arq) as f:
+                    temp = pd.read_csv(f, encoding='Latin-1', delimiter=';')
+                    df = pd.concat([df, temp])
+
+    return df
+
+
+def processa_dados_financeiros(tipo):
+
+    balanco_ativo = processa_base_cvm(tipo, 'BPA_con')
+    balanco_passivo = processa_base_cvm(tipo, 'BPP_con')
+    dre = processa_base_cvm(tipo, 'DRE_con')
+    dra = processa_base_cvm(tipo, 'DRA_con')
+    dfc_md = processa_base_cvm(tipo, 'DFC_MD_con')
+    dfc_mi = processa_base_cvm(tipo, 'DFC_MI_con')
+
+    df = pd.concat([balanco_ativo, balanco_passivo, dre, dra, dfc_md, dfc_mi])
+
+    df = df[df.ORDEM_EXERC == 'ÚLTIMO']       
+    df.VL_CONTA = df.VL_CONTA.astype(float)
+    df['ano'] = df['DT_REFER'].str[:4].astype(int)
+
+    df['tipo'] = tipo
+
+    df.columns = ['cnpj', 'dt_ref', 'versao', 'nome', 'cod_cvm', 'grupo_dfp', 'moeda', 'escala_moeda',
+                  'ordem_exerc', 'dt_fim_exerc', 'cod_conta', 'desc_conta', 'valor', 'sit_conta_fixa',
+                  'dt_ini_exerc', 'ano', 'form']
+
+    df.dt_ref = pd.to_datetime(df.dt_ref)
+
+    return df
+
+
+def cria_base_Dados_Financeiros():
+
+
+    base_download_cvm = ['DFP', 'ITR', 'FRE', 'FCA']
+
+    for tipo in base_download_cvm:
+        print(tipo)
+        download_arquivos_CVM(tipo)
+
+    # Atualiza data do último download
+
+    data_ultimo_download = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    with open('controle_download.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([data_ultimo_download])
+
+    # Gera cadastro
+
+    cadastro = cria_cadastro()
+
+    # Dados Financeiros
+
+    # Processa DFP
+
+    dfp = processa_dados_financeiros('DFP')
+
+    ultimo_ano_dfp = dfp.groupby(['cod_cvm']).last().reset_index()[['cod_cvm', 'dt_ref', 'ano']]
+    ultimo_ano_dfp.columns = ['cod_cvm', 'ultimo_dfp_dt_ref', 'ultimo_dfp_ano']
+
+    ano_anterior = dfp.ano.max() - 1
+
+    empresas_ano_anterior = ultimo_ano_dfp[ultimo_ano_dfp.ultimo_dfp_ano >= ano_anterior]
+    dfp = dfp[dfp.cod_cvm.isin(empresas_ano_anterior.cod_cvm)]
+
+    # Processa ITR (deixa somente último ITR se houver)
+
+    itr = processa_dados_financeiros('ITR')
+
+    ultimo_itr = itr.groupby(['cod_cvm']).last().reset_index()[['cod_cvm', 'ano', 'dt_ref', 'versao']]
+    ultimo_itr = ultimo_itr.merge(ultimo_ano_dfp, on='cod_cvm', how='left')
+
+    ultimo_itr = ultimo_itr[ultimo_itr.dt_ref > ultimo_itr.ultimo_dfp_dt_ref]
+    ultimo_itr = ultimo_itr[((ultimo_itr.ultimo_dfp_dt_ref.isna()) & (ultimo_itr.ano > ano_anterior)) |
+                            (ultimo_itr.ano > ano_anterior)]
+
+
+    itr['chave'] = itr.cod_cvm.astype(str) + itr.dt_ref.dt.strftime('%Y-%m-%d') + itr.versao.astype(str)
+    ultimo_itr['chave'] = ultimo_itr.cod_cvm.astype(str) + ultimo_itr.dt_ref.dt.strftime('%Y-%m-%d') + ultimo_itr.versao.astype(str)
+
+    itr = itr[itr.chave.isin(ultimo_itr.chave)]
+
+    del itr['chave']
+
+
+    # Junta DFP com último ITR
+    financ = pd.concat([dfp, itr])
+
+    # Seleciona saldos de interesse
+
+    contas_selec = ['1', '1.01.01', '1.01.02', '2.03', '3.01', '3.03',
+                    '3.05', '3.11', '2.01.04', '2.02.01']
+
+    # idx saldos
+    idx_saldos = financ.cod_conta.isin(contas_selec)
+
+    # idx deprec
+    idx_deprec = (financ.cod_conta.str.startswith('6.01')
+                ) & (
+                financ.desc_conta.str.lower().str.contains('deprec|amortiz', regex=True))
+
+
+    saldos = financ[idx_saldos]
+
+    deprec = financ[idx_deprec]
+    deprec = deprec.groupby(['form', 'cod_cvm', 'ano', 'dt_ref']).sum('valor').reset_index()
+    deprec['deprec_amortiz'] = deprec['valor']
+    del deprec['valor']
+
+    # Gera arquivo de saída
+
+    df = saldos.pivot_table(values='valor', index=['form', 'cod_cvm', 'ano', 'dt_ref'], columns='cod_conta', fill_value=0).reset_index()
+
+    df = df.merge(deprec, on=['form', 'cod_cvm', 'ano', 'dt_ref'], how='left')
+    df = df.fillna(0)
+
+    df['ativo'] = df['1']
+    df['caixa'] = df['1.01.01'] + df['1.01.02']
+    df['divida_curto_prazo'] = df['2.01.04']
+    df['divida_longo_prazo'] = df['2.02.01']
+    df['divida_total'] = df['divida_curto_prazo'] + df['divida_longo_prazo']
+    df['patr_liq'] = df['2.03']
+    df['receita_liq'] = df['3.01']
+    df['lucro_bruto'] = df['3.03']
+    df['lucro_liq'] = df['3.11']
+    df['EBIT'] = df['3.05']
+
+    df['endivid_taxa'] = round(df['divida_total'] / df['ativo'], 2)
+    df['margem_liq'] = round(df['lucro_liq'] / df['receita_liq'], 4)
+    df['EBITDA'] = round(df['EBIT'] + df['deprec_amortiz'], 2)
+    df['divida_liq'] = round((df['divida_total'] - df['caixa']) / df['EBITDA'], 2)
+
+    df = df[['form', 'cod_cvm', 'ano', 'dt_ref',
+        'ativo', 'patr_liq', 'receita_liq', 'lucro_bruto', 'lucro_liq', 'EBIT',
+        'divida_curto_prazo', 'divida_longo_prazo', 'caixa', 'divida_total',
+        'endivid_taxa', 'margem_liq', 'deprec_amortiz', 'EBITDA', 'divida_liq']]
+
+
+    df = df.merge(cadastro, on='cod_cvm')
+
+    df = df[['segmento', 'nome', 'cod_cvm', 'site', 'ticker', 'ano', 'form', 'dt_ref',
+        'ativo', 'patr_liq', 'receita_liq', 'lucro_bruto', 'lucro_liq', 'EBIT',
+        'deprec_amortiz', 'EBITDA', 'margem_liq', 'divida_curto_prazo', 'divida_longo_prazo',
+        'caixa', 'divida_liq', 'divida_total', 'acoes', 'free_float', 'governanca']]
+
+
+    df = df.sort_values(by=['nome', 'ano'])
+
+    df.to_csv('DadosFinanceiros.csv', sep=';', decimal=',', index=False, encoding='Latin1')
+
+
+
+# Procedimento Principal
+
+st.set_page_config(
+    layout='wide',
+    page_icon='app.jpg',
+    page_title='B3')
+
+
+data_hoje = datetime.today().strftime('%Y-%m-%d')
+
+with open('controle_base_dados_financeiros.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        data_base_dados_financeiros = row[0]
+
+if data_hoje > data_base_dados_financeiros:
+
+    with st.spinner('Processando base Dados Financeiros'):
+        cria_base_Dados_Financeiros()
+
+        # Atualiza data da criação da base
+        with open('controle_base_dados_financeiros.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([data_hoje])
+
+
+f = 'DadosFinanceiros.csv'
 
 financ = readDadosFinanceiros(f)
 
@@ -50,31 +365,28 @@ row1_1, row1_2 = st.columns([1.3, 3])
 
 with row1_1:
     # Prepara lista de empresas
-    ticker_opcoes =  financ.segmento.str[:20] + ' - ' + financ.ticker.str[:4] + ' - ' + financ.nome
+    ticker_opcoes = financ.nome + ' ; ' + financ.ticker.str[:4]
     ticker_opcoes = ticker_opcoes.drop_duplicates().sort_values()
 
     ticker_selecionado = st.selectbox('Selecione a empresa:', ticker_opcoes )
-    ticker = ticker_selecionado.split(sep='-')[1].strip()
-    empresa = ticker_selecionado.split(sep='-')[2].strip()
+    ticker = ticker_selecionado.split(sep=';')[1].strip()
+    empresa = ticker_selecionado.split(sep=';')[0].strip()
 
-# FILTERING DATA (limitando aos 12 últimos anos - tail)
-df = financ[financ.ticker.str.startswith(str.upper(ticker))].tail(12).copy()
+# FILTERING DATA (limitando aos 10 últimos anos - tail)
+df = financ[financ.ticker.str.startswith(str.upper(ticker))].tail(10).copy()
 print(df)
 
 qtd_acoes = df.acoes.iloc[0]
-ult_dem = df.form.iloc[-1]
-ult_dem_data = df.dt_ref.iloc[-1]
 
 # Define para merge do cálculo do P/L diário
 df['prox_ano'] = df.ano + 1
 
-df_aux = df[['ano', 'form', 'receita_liq', 'lucro_liq', 'margem_liq', 'EBITDA', 'divida_liq', 'caixa', 'patr_liq', 'divida_total']]
-df_aux = df_aux.tail(9)
+df_aux = df[['ano', 'form', 'receita_liq', 'lucro_liq', 'margem_liq', 'EBITDA', 'divida_liq', 'caixa', 'patr_liq', 'divida_total', 'dt_ref']]
 
 df_aux.reset_index(inplace=True, drop=True) 
 df_aux = df_aux.set_index('ano')
 
-df_aux.columns = ['Dem', 'Rec.Líq', 'Luc.Líq', 'Marg.Líq', 'EBITDA', 'Dív.Líq', 'Caixa', 'Patr.Líq', 'Dív.Total']
+df_aux.columns = ['Dem', 'Rec.Líq', 'Luc.Líq', 'Marg.Líq', 'EBITDA', 'Dív.Líq', 'Caixa', 'Patr.Líq', 'Dív.Total', 'Data']
 
 df_aux = df_aux.style.format(thousands=".",
                              decimal = ",",
@@ -89,12 +401,6 @@ df_aux = df_aux.style.format(thousands=".",
 
 
 # EXIBE DATAFRAME
-with row1_2:
-    st.table(df_aux)
-    st.write('DFP: Demonstrações Financeiras Padronizadas (anual) / ITR: Informações Trimestrais')
-    if ult_dem == 'ITR':
-        st.write(f'ITR -> dados acumulados até {ult_dem_data}')
-
 with row1_1:
     #st.write(f'{df.ticker.iloc[0]} - {df.pregao.iloc[0]}')
     st.write(f'{df.nome.iloc[0]}')
@@ -106,6 +412,9 @@ with row1_1:
     if site[0:4] != 'http' and site != '':
         site = 'http://' + site
     st.write(site)
+
+with row1_2:
+    st.table(df_aux)
 
 
 fig = make_subplots(rows=2, cols=2, 
@@ -126,7 +435,7 @@ fig.add_trace(
     secondary_y=False,
     row=1, col=2)
 fig.add_trace(
-    go.Scatter(x=df.ano, y=df.margem_liq, marker=dict(color="crimson"), name='Margem Líquida'), 
+    go.Scatter(x=df.ano, y=df.margem_liq*100, marker=dict(color="crimson"), name='Margem Líquida'), 
     secondary_y=True,
     row=1, col=2)
 
